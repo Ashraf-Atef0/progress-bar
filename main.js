ef7e9dd9dd0895f13ce4c3a9711ba8689b169959
Ashraf Atef <ashraf.programer@gmail.com>
2023-07-20 09:08:55 +0300

    upload project V1.0

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log("test");
+let progressBar = document.querySelector(".progress-bar");
+let circles = document.querySelectorAll(".circle");
+let prevBtn = document.getElementById("prev");
+let nextBtn = document.getElementById("next");
+let activeNum = 1;
+
+function next() {
+  activeNum++;
+  if (activeNum > circles.length) {
+    activeNum = circles.length;
+  }
+  updateCircles();
+}
+nextBtn.addEventListener("click", next);
+function prev() {
+  activeNum--;
+  if (activeNum < 1) {
+    activeNum = 1;
+  }
+  updateCircles();
+}
+prevBtn.addEventListener("click", prev);
+function updateCircles() {
+  circles.forEach((e, i) => {
+    if (i < activeNum) {
+      e.classList.add("active");
+    } else {
+      e.classList.remove("active");
+    }
+  });
+
+  progressBar.style.width =
+    (100 * (activeNum - 1)) / (circles.length - 1) + "%";
+  console.log(progressBar.style.width);
+  if (activeNum == 1) {
+    prevBtn.disabled = true;
+  } else if (activeNum == circles.length) {
+    nextBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+}
